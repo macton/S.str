bc30a353d1d28334e7475b4d6a958c01836c4728
Mike Acton <macton@gmail.com>
2015-07-02 11:15:36 -0700

    null string tests

--- s.str.js
+++ s.str.js
@@ -3,9 +3,9 @@ var S = require('s');
 var str = {
   isSub      : S.curry( function( a, b ) { return a.indexOf(b) >= 0; } ),
   indexOf    : S.curry( function( a, b ) { return a.indexOf(b); } ),
-  length     : function( a ) { return a.length },
-  isEmpty    : function( a ) { return a.length === 0; },
-  trim       : function( a ) { return a.trim(); },
+  length     : function( a ) { return a?a.length:0 },
+  isEmpty    : function( a ) { return a?(a.length === 0):true; },
+  trim       : function( a ) { return a?a.trim():''; },
   join       : S.curry( function( delim, list ) { return list.join(delim); } ),
   concat     : function( list ) { return list.join(''); },
   prefix     : S.curry( function( p, a ) { return p+a; }),
